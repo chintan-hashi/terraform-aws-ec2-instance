000ff9fb30d93bfe5743ca814e7a4b96dd1e2397
chintan-hashi <57116381+chintan-hashi@users.noreply.github.com>
2021-03-03 23:35:12 -0800

    new version

--- outputs.tf
+++ outputs.tf
@@ -8,25 +8,6 @@ output "arn" {
   value       = aws_instance.this.*.arn
 }
 
-output "availability_zone" {
-  description = "List of availability zones of instances"
-  value       = aws_instance.this.*.availability_zone
-}
-
-output "placement_group" {
-  description = "List of placement groups of instances"
-  value       = aws_instance.this.*.placement_group
-}
-
-output "key_name" {
-  description = "List of key names of instances"
-  value       = aws_instance.this.*.key_name
-}
-
-output "password_data" {
-  description = "List of Base-64 encoded encrypted password data for the instance"
-  value       = aws_instance.this.*.password_data
-}
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances. For EC2-VPC, this is only available if you've enabled DNS hostnames for your VPC"
@@ -36,21 +17,6 @@ output "public_dns" {
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances, if applicable"
   value       = aws_instance.this.*.public_ip
-}
-
-output "ipv6_addresses" {
-  description = "List of assigned IPv6 addresses of instances"
-  value       = aws_instance.this.*.ipv6_addresses
-}
-
-output "primary_network_interface_id" {
-  description = "List of IDs of the primary network interface of instances"
-  value       = aws_instance.this.*.primary_network_interface_id
-}
-
-output "private_dns" {
-  description = "List of private DNS names assigned to the instances. Can only be used inside the Amazon EC2, and only available if you've enabled DNS hostnames for your VPC"
-  value       = aws_instance.this.*.private_dns
 }
 
 output "private_ip" {
@@ -73,42 +39,7 @@ output "subnet_id" {
   value       = aws_instance.this.*.subnet_id
 }
 
-output "credit_specification" {
-  description = "List of credit specification of instances"
-  value       = aws_instance.this.*.credit_specification
-}
-
-output "metadata_options" {
-  description = "List of metadata options of instances"
-  value       = aws_instance.this.*.metadata_options
-}
-
 output "instance_state" {
   description = "List of instance states of instances"
   value       = aws_instance.this.*.instance_state
-}
-
-output "root_block_device_volume_ids" {
-  description = "List of volume IDs of root block devices of instances"
-  value       = [for device in aws_instance.this.*.root_block_device : device.*.volume_id]
-}
-
-output "ebs_block_device_volume_ids" {
-  description = "List of volume IDs of EBS block devices of instances"
-  value       = [for device in aws_instance.this.*.ebs_block_device : device.*.volume_id]
-}
-
-output "tags" {
-  description = "List of tags of instances"
-  value       = aws_instance.this.*.tags
-}
-
-output "volume_tags" {
-  description = "List of tags of volumes of instances"
-  value       = aws_instance.this.*.volume_tags
-}
-
-output "instance_count" {
-  description = "Number of instances to launch specified as argument to this module"
-  value       = var.instance_count
 }
